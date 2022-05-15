6728ffb0ef4160f9128be5601e0ed3aa3f67464d
Mitch <MitchelClark1997@outlook.com>
2022-05-15 15:47:20 +1000

    init commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :books
+  root 'pages#home'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
