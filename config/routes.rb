9bff0574c2d07ff953e72e78c4cff769d1ae8f05
Mitch <MitchelClark1997@outlook.com>
2022-05-22 13:04:30 +1000

    Add posts table to db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   use_doorkeeper
   devise_for :users
   resources :books
--- config/routes/api.rb
+++ config/routes/api.rb
@@ -4,6 +4,7 @@ namespace :api do
       post '/', to: 'registrations#create', as: :user_registration
     end
     resources :books
+    resources :posts
 
     namespace :android do
       resources :books
